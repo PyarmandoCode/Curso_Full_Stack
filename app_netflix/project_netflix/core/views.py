f6d79af9c22a9a42a6dd183c3ffe3e30fad907fa
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-02-08 10:51:16 -0500

    boostratp

--- app_netflix/project_netflix/core/views.py
+++ app_netflix/project_netflix/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 #vista basada en funcion que me permite renderizar
 #a mi template(html)
 def index(request):
-    template_name="index.html"
+    template_name="menu.html"
     return render(request,template_name)
 
 
