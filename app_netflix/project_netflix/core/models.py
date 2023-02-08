c686118209ccf00d9437e9e68cd77c78a959966f
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-02-08 11:08:53 -0500

    boostratp

--- app_netflix/project_netflix/core/models.py
+++ app_netflix/project_netflix/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class peliculas(models.Model):
+    reseña = models.CharField(max_length=50)
+    puntuacion = models.CharField(max_length=20)
+    director = models.CharField(max_length=40)
+    descripcion = models.TextField(blank=True)
+    contenido = models.CharField(max_length=40,blank=True)
+    imagen= models.ImageField(upload_to="tapas",blank=True)
+    estado = models.BooleanField(default=True)
+    sipnosis = models.TextField(blank=True)
+    genero = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.reseña 
+    
--- app_netflix/project_netflix/core/views.py
+++ app_netflix/project_netflix/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 #vista basada en funcion que me permite renderizar
 #a mi template(html)
 def index(request):
-    template_name="menu.html"
+    template_name="index.html"
     return render(request,template_name)
 
 
