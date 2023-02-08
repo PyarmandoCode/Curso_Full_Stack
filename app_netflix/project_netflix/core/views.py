b2b7619ed90b0051bd6037d036fc9695f71dd397
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-02-08 12:43:28 -0500

    boostratp

--- app_netflix/project_netflix/core/views.py
+++ app_netflix/project_netflix/core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import peliculas
 
 #vista basada en funcion que me permite renderizar
 #a mi template(html)
 def index(request):
     template_name="index.html"
-    return render(request,template_name)
+    pls=peliculas.objects.all()
+    context = {
+        "peliculas":pls
+    }
+    return render(request,template_name,context)
 
 
