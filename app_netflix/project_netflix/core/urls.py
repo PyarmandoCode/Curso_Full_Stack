7ab7a4e51e1f8801e01f5452a26058d90ad388cc
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-02-09 10:23:44 -0500

    rutas

--- app_netflix/project_netflix/core/urls.py
+++ app_netflix/project_netflix/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import index,buscar,detalles
 
 app_name="core"
 
 urlpatterns = [
     path('', index,name="inicio"),
+    path('buscar/', buscar,name="search"),
+    path('detalles/<int:id_pelicula>', detalles,name="detail"),
 ]
--- app_netflix/project_netflix/core/views.py
+++ app_netflix/project_netflix/core/views.py
@@ -5,10 +5,39 @@ from .models import peliculas
 #a mi template(html)
 def index(request):
     template_name="index.html"
+    #select * from peliculas
     pls=peliculas.objects.all()
     context = {
         "peliculas":pls
     }
     return render(request,template_name,context)
+
+def buscar(request):
+    template_name="index.html"
+    if request.method=="GET":
+        #Guardando en una variable el valos del input
+        query=request.GET.get('q')
+        #filtrando de la tabla peliculas de acuerdo a la variable
+        results = peliculas.objects.filter(reseña__icontains=query)
+        #creando un contexto con el resultado del filtro
+        context = {
+            "peliculas":results
+        }
+        #renderizando en el template
+    return render(request,template_name,context)    
+
+
+def detalles(request,id_pelicula=None):
+    template_name="detalles.html"
+    #select * from peliculas where id=?
+    pel = peliculas.objects.get(pk=id_pelicula)
+    context = {
+        "pelicula":pel
+    }
+    return render(request,template_name,context)
+
+
+
+
 
 
