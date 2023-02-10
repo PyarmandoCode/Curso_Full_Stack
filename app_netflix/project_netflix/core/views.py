98873b8e6f81929a265ae20aa4193c2b5f721033
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-02-10 10:03:38 -0500

    actualizar

--- app_netflix/project_netflix/core/views.py
+++ app_netflix/project_netflix/core/views.py
@@ -1,67 +1,85 @@
 from django.shortcuts import render
-from .models import peliculas,genero
+from .models import peliculas, genero
 from django.db.models import Q
 
-#vista basada en funcion que me permite renderizar
-#a mi template(html)
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+# vista basada en funcion que me permite renderizar
+# a mi template(html)
 def index(request):
-    template_name="index.html"
-    #select * from peliculas
-    pls=peliculas.objects.all()
+    template_name = "index.html"
+    # select * from peliculas
+    pls = peliculas.objects.all()
     context = {
-        "peliculas":pls
+        "peliculas": pls
     }
-    return render(request,template_name,context)
+    return render(request, template_name, context)
+
 
 def buscar(request):
-    template_name="index.html"
-    if request.method=="GET":
-        #Guardando en una variable el valos del input
-        query=request.GET.get('q')
-        #filtrando de la tabla peliculas de acuerdo a la variable
+    template_name = "index.html"
+    if request.method == "GET":
+        # Guardando en una variable el valos del input
+        query = request.GET.get('q')
+        # filtrando de la tabla peliculas de acuerdo a la variable
         results = peliculas.objects.filter(Q(reseña__icontains=query) | Q(director__icontains=query))
-        #creando un contexto con el resultado del filtro
+        # creando un contexto con el resultado del filtro
         context = {
-            "peliculas":results
+            "peliculas": results
         }
-        #renderizando en el template
-    return render(request,template_name,context)    
+        # renderizando en el template
+    return render(request, template_name, context)
 
 
-def detalles(request,id_pelicula=None):
-    template_name="detalles.html"
-    #select * from peliculas where id=?
+def detalles(request, id_pelicula=None):
+    template_name = "detalles.html"
+    # select * from peliculas where id=?
     pel = peliculas.objects.get(pk=id_pelicula)
     context = {
-        "pelicula":pel
+        "pelicula": pel
     }
-    return render(request,template_name,context)
+    return render(request, template_name, context)
+
 
 def crear(request):
-    template_name="create.html"
-    if request.method=="POST":
-        vargenero=request.POST['genero']
-        #print(vargenero)
-        #todo creando el Objeto para el FK
-        obj_genero=genero.objects.get(codgenero=vargenero)
+    template_name = "create.html"
+    if request.method == "POST":
+        vargenero = request.POST['genero']
+        # print(vargenero)
+        # todo creando el Objeto para el FK
+        obj_genero = genero.objects.get(codgenero=vargenero)
         pelicula = peliculas(reseña=request.POST['reseña'],
-        descripcion =request.POST["descripcion"],
-        sipnosis=request.POST["sipnosis"],
-        director=request.POST["director"],
-        puntuacion=request.POST["puntuacion"],
-        #dato del FK pasarle todo el objeto
-        genero=obj_genero
-        )
+                             descripcion=request.POST["descripcion"],
+                             sipnosis=request.POST["sipnosis"],
+                             director=request.POST["director"],
+                             puntuacion=request.POST["puntuacion"],
+                             # dato del FK pasarle todo el objeto
+                             genero=obj_genero
+                             )
         pelicula.save()
-    generos=genero.objects.all()
+        return HttpResponseRedirect(reverse('core:inicio'))
+    generos = genero.objects.all()
     context = {
-        "generos":generos
+        "generos": generos
     }
-    return render(request,template_name,context)
+    return render(request, template_name, context)
 
 
+def eliminar(request, id_pelicula=None):
+    peliculas.objects.get(pk=id_pelicula).delete()
+    return HttpResponseRedirect(reverse('core:inicio'))
 
 
+def editar(request,id_pelicula):
+    template_name = "edit.html"
+    pelicula = peliculas.objects.get(pk=id_pelicula)
+    generos = genero.objects.all()
 
+    context = {
+        "pelicula":pelicula,
+        "generos": generos
 
-
+    }
+    return render(request, template_name, context)
